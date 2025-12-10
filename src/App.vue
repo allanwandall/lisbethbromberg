9951ef4a9924f8e02545e4f0d4c4dca2ac03c77d
w34294 <Allan.Wandall@ufst.dk>
2025-12-10 22:26:50 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-container">
+    <h1>Lisbeth Bromberg Terapi</h1>
+    <img src="./assets/maelk.webp" alt="Lad os dele et glas mælk" />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+img {
+  width: 300px;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
