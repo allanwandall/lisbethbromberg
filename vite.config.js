f708f15af5aaf2d854654857b0e8212ca512acbe
Allan Wandall <allanwandall@gmail.com>
2026-02-01 02:05:37 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lisbethbromberg/',
+  //base: '/lisbethbromberg/',
+  base: "",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
